5169f1dec2c46013a2df5816ac8477877b97d592
Salvatore <ssprog@gmail.com>
2022-01-31 22:50:14 +0100

    prove di gltf

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,25 +1,127 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
+const light1 = new THREE.PointLight(0xffffff, 2)
+light1.position.set(2.5, 2.5, 2.5)
+scene.add(light1)
+
+const light2 = new THREE.PointLight(0xffffff, 2)
+light2.position.set(-2.5, 2.5, 2.5)
+scene.add(light2)
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+camera.position.set(0.8, 1.4, 1.0)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.target.set(0, 1, 0)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+let mixer: THREE.AnimationMixer
+let modelReady = false
+const animationActions: THREE.AnimationAction[] = []
+let activeAction: THREE.AnimationAction
+let lastAction: THREE.AnimationAction
+const gltfLoader = new GLTFLoader()
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+gltfLoader.load(
+    'models/vanguard.glb',
+    (gltf) => {
+        // gltf.scene.scale.set(.01, .01, .01)
+
+        mixer = new THREE.AnimationMixer(gltf.scene)
+
+        const animationAction = mixer.clipAction((gltf as any).animations[0])
+        animationActions.push(animationAction)
+        animationsFolder.add(animations, 'default')
+        activeAction = animationActions[0]
+
+
+        scene.add(gltf.scene)
+
+        //add an animation from another file
+        gltfLoader.load(
+            'models/vanguard@samba.glb',
+            (gltf) => {
+                console.log('loaded samba')
+                const animationAction = mixer.clipAction(
+                    (gltf as any).animations[0]
+                )
+                animationActions.push(animationAction)
+                animationsFolder.add(animations, 'samba')
+
+                //add an animation from another file
+                gltfLoader.load(
+                    'models/vanguard@bellydance.glb',
+                    (gltf) => {
+                        console.log('loaded bellydance')
+                        const animationAction = mixer.clipAction(
+                            (gltf as any).animations[0]
+                        )
+                        animationActions.push(animationAction)
+                        animationsFolder.add(animations, 'bellydance')
+
+                        //add an animation from another file
+                        gltfLoader.load(
+                            'models/vanguard@goofyrunning.glb',
+                            (gltf) => {
+                                console.log('loaded goofyrunning');
+                                (gltf as any).animations[0].tracks.shift() //delete the specific track that moves the object forward while running
+                                const animationAction = mixer.clipAction(
+                                    (gltf as any).animations[0]
+                                )
+                                animationActions.push(animationAction)
+                                animationsFolder.add(animations, 'goofyrunning')
+
+                                modelReady = true
+                            },
+                            (xhr) => {
+                                console.log(
+                                    (xhr.loaded / xhr.total) * 100 + '% loaded'
+                                )
+                            },
+                            (error) => {
+                                console.log(error)
+                            }
+                        )
+                    },
+                    (xhr) => {
+                        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                    },
+                    (error) => {
+                        console.log(error)
+                    }
+                )
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+            },
+            (error) => {
+                console.log(error)
+            }
+        )
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -29,18 +131,56 @@ function onWindowResize() {
     render()
 }
 
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+const animations = {
+    default: function () {
+        setAction(animationActions[0])
+    },
+    samba: function () {
+        setAction(animationActions[1])
+    },
+    bellydance: function () {
+        setAction(animationActions[2])
+    },
+    goofyrunning: function () {
+        setAction(animationActions[3])
+    }
+}
+
+const setAction = (toAction: THREE.AnimationAction) => {
+    if (toAction != activeAction) {
+        lastAction = activeAction
+        activeAction = toAction
+        //lastAction.stop()
+        lastAction.fadeOut(1)
+        activeAction.reset()
+        activeAction.fadeIn(1)
+        activeAction.play()
+    }
+}
+
+const gui = new GUI()
+const animationsFolder = gui.addFolder('Animations')
+animationsFolder.open()
+
+const clock = new THREE.Clock()
+
 function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
     controls.update()
 
+    if (modelReady) mixer.update(clock.getDelta())
+
     render()
+
+    stats.update()
 }
 
 function render() {
     renderer.render(scene, camera)
 }
+
 animate()
