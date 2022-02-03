e99b8ba8ab20d32561e4a7f89cb32af79ce92ccd
Salvatore <ssprog@gmail.com>
2022-02-03 15:02:58 +0100

    fix div container e css

--- src/client/main.ts
+++ src/client/main.ts
@@ -6,49 +6,55 @@ import {
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
-} from 'three';
+} from 'three'
+
+// Get a reference to the container element that will hold our scene
+const container = document.querySelector('#scene-container')
+if (!(container instanceof HTMLDivElement)) {
+  throw new Error('scene-container div not found in dom')
+}
 
 // create a Scene
-const scene = new Scene();
+const scene = new Scene()
 
 // Set the background color
-scene.background = new Color('skyblue');
+scene.background = new Color('skyblue')
 
 // Create a camera
-const fov = 35; // AKA Field of View
-const aspect = window.innerWidth / window.innerHeight;
-const near = 0.1; // the near clipping plane
-const far = 100; // the far clipping plane
+const fov = 35 // AKA Field of View
+const aspect = container.clientWidth / container.clientHeight
+const near = 0.1 // the near clipping plane
+const far = 100 // the far clipping plane
 
-const camera = new PerspectiveCamera(fov, aspect, near, far);
+const camera = new PerspectiveCamera(fov, aspect, near, far)
 
 // every object is initially created at ( 0, 0, 0 )
 // move the camera back so we can view the scene
-camera.position.set(0, 0, 10);
+camera.position.set(0, 0, 10)
 
 // create a geometry
-const geometry = new BoxBufferGeometry(2, 2, 2);
+const geometry = new BoxBufferGeometry(2, 2, 2)
 
 // create a default (white) Basic material
-const material = new MeshBasicMaterial();
+const material = new MeshBasicMaterial()
 
 // create a Mesh containing the geometry and material
-const cube = new Mesh(geometry, material);
+const cube = new Mesh(geometry, material)
 
 // add the mesh to the scene
-scene.add(cube);
+scene.add(cube)
 
 // create the renderer
 const renderer = new WebGLRenderer()
 
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+// add the automatically created <canvas> element to the page
+container.append(renderer.domElement)
 
-// next, set the renderer to the same size as our window element
-renderer.setSize(window.innerWidth, window.innerHeight);
+// next, set the renderer to the same size as our container element
+renderer.setSize(container.clientWidth, container.clientHeight)
 
 // finally, set the pixel ratio so that our scene will look good on HiDPI displays
-renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setPixelRatio(window.devicePixelRatio)
 
 // render, or 'create a still image', of the scene
-renderer.render(scene, camera);
+renderer.render(scene, camera)
