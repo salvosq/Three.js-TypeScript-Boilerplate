36d2f4abe3640ef355b8f7b6f964c4cea452adf6
Salvatore <ssprog@gmail.com>
2022-02-03 10:50:59 +0100

    primo hello world

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,186 +1,57 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import Stats from 'three/examples/jsm/libs/stats.module'
-import { GUI } from 'dat.gui'
+import {
+  BoxBufferGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from 'three';
 
-const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
+// Get a reference to the container element that will hold our scene
+const container = document.querySelector('#scene-container');
 
-const light1 = new THREE.PointLight(0xffffff, 2)
-light1.position.set(2.5, 2.5, 2.5)
-scene.add(light1)
+// create a Scene
+const scene = new Scene();
 
-const light2 = new THREE.PointLight(0xffffff, 2)
-light2.position.set(-2.5, 2.5, 2.5)
-scene.add(light2)
+// Set the background color
+scene.background = new Color('skyblue');
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.set(0.8, 1.4, 1.0)
+// Create a camera
+const fov = 35; // AKA Field of View
+const aspect = window.innerWidth / window.innerHeight;
+const near = 0.1; // the near clipping plane
+const far = 100; // the far clipping plane
 
-const renderer = new THREE.WebGLRenderer()
+const camera = new PerspectiveCamera(fov, aspect, near, far);
+
+// every object is initially created at ( 0, 0, 0 )
+// move the camera back so we can view the scene
+camera.position.set(0, 0, 10);
+
+// create a geometry
+const geometry = new BoxBufferGeometry(2, 2, 2);
+
+// create a default (white) Basic material
+const material = new MeshBasicMaterial();
+
+// create a Mesh containing the geometry and material
+const cube = new Mesh(geometry, material);
+
+// add the mesh to the scene
+scene.add(cube);
+
+// create the renderer
+const renderer = new WebGLRenderer()
+
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-controls.target.set(0, 1, 0)
+// next, set the renderer to the same size as our container element
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-let mixer: THREE.AnimationMixer
-let modelReady = false
-const animationActions: THREE.AnimationAction[] = []
-let activeAction: THREE.AnimationAction
-let lastAction: THREE.AnimationAction
-const gltfLoader = new GLTFLoader()
+// finally, set the pixel ratio so that our scene will look good on HiDPI displays
+renderer.setPixelRatio(window.devicePixelRatio);
 
-gltfLoader.load(
-    'models/anima-bona-001.glb',
-    (gltf) => {
-        // gltf.scene.scale.set(.01, .01, .01)
-
-        mixer = new THREE.AnimationMixer(gltf.scene)
-
-        const animationAction = mixer.clipAction((gltf as any).animations[0])
-        animationActions.push(animationAction)
-        animationsFolder.add(animations, 'default')
-        activeAction = animationActions[0]
-
-
-        scene.add(gltf.scene)
-
-        //add an animation from another file
-        gltfLoader.load(
-            'models/anima-bona-001@samba.glb',
-            (gltf) => {
-                console.log('loaded samba')
-                const animationAction = mixer.clipAction(
-                    (gltf as any).animations[0]
-                )
-                animationActions.push(animationAction)
-                animationsFolder.add(animations, 'samba')
-
-                //add an animation from another file
-                gltfLoader.load(
-                    'models/anima-bona-001@bellydance.glb',
-                    (gltf) => {
-                        console.log('loaded bellydance')
-                        const animationAction = mixer.clipAction(
-                            (gltf as any).animations[0]
-                        )
-                        animationActions.push(animationAction)
-                        animationsFolder.add(animations, 'bellydance')
-
-                        //add an animation from another file
-                        gltfLoader.load(
-                            'models/anima-bona-001@goofyrunning.glb',
-                            (gltf) => {
-                                console.log('loaded goofyrunning');
-                                (gltf as any).animations[0].tracks.shift() //delete the specific track that moves the object forward while running
-                                const animationAction = mixer.clipAction(
-                                    (gltf as any).animations[0]
-                                )
-                                animationActions.push(animationAction)
-                                animationsFolder.add(animations, 'goofyrunning')
-
-                                modelReady = true
-                            },
-                            (xhr) => {
-                                console.log(
-                                    (xhr.loaded / xhr.total) * 100 + '% loaded'
-                                )
-                            },
-                            (error) => {
-                                console.log(error)
-                            }
-                        )
-                    },
-                    (xhr) => {
-                        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-                    },
-                    (error) => {
-                        console.log(error)
-                    }
-                )
-            },
-            (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-            },
-            (error) => {
-                console.log(error)
-            }
-        )
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
-
-const stats = Stats()
-document.body.appendChild(stats.dom)
-
-const animations = {
-    default: function () {
-        setAction(animationActions[0])
-    },
-    samba: function () {
-        setAction(animationActions[1])
-    },
-    bellydance: function () {
-        setAction(animationActions[2])
-    },
-    goofyrunning: function () {
-        setAction(animationActions[3])
-    }
-}
-
-const setAction = (toAction: THREE.AnimationAction) => {
-    if (toAction != activeAction) {
-        lastAction = activeAction
-        activeAction = toAction
-        //lastAction.stop()
-        lastAction.fadeOut(1)
-        activeAction.reset()
-        activeAction.fadeIn(1)
-        activeAction.play()
-    }
-}
-
-const gui = new GUI()
-const animationsFolder = gui.addFolder('Animations')
-animationsFolder.open()
-
-const clock = new THREE.Clock()
-
-function animate() {
-    requestAnimationFrame(animate)
-
-    controls.update()
-
-    if (modelReady) mixer.update(clock.getDelta())
-
-    render()
-
-    stats.update()
-}
-
-function render() {
-    renderer.render(scene, camera)
-}
-
-animate()
+// render, or 'create a still image', of the scene
+renderer.render(scene, camera);
