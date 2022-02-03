4760b75ceb1fc3394781b452e21c753487e1b33b
Salvatore <ssprog@gmail.com>
2022-02-03 15:09:47 +0100

    rifinito primo hello world

--- src/client/main.ts
+++ src/client/main.ts
@@ -47,14 +47,14 @@ scene.add(cube)
 // create the renderer
 const renderer = new WebGLRenderer()
 
-// add the automatically created <canvas> element to the page
-container.append(renderer.domElement)
-
 // next, set the renderer to the same size as our container element
 renderer.setSize(container.clientWidth, container.clientHeight)
 
 // finally, set the pixel ratio so that our scene will look good on HiDPI displays
 renderer.setPixelRatio(window.devicePixelRatio)
+
+// add the automatically created <canvas> element to the page
+container.append(renderer.domElement)
 
 // render, or 'create a still image', of the scene
 renderer.render(scene, camera)
